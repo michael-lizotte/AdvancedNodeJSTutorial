5b809f2c1e5e083e8531673afe3cbf02377323be
Michael Lizotte-Gagnon <michael.lizotte-gagnon@cgi.com>
2019-05-31 13:44:57 -0400

    Added .travis.yaml

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://localhost:27017/blog-local',
+  cookieKey: '123123123',
+  redisURI: 'redis://localhost:6379',
+  redisPassword: ''
+};
